b9dafd672cd4bb3a99a40d45f836777b14468b0b
aliabdelnaby <aliabdelnabi239@gmail.com>
2024-08-10 17:31:18 +0300

    App Colors

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color black = Color(0xFF000000);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color primary = Color(0xFF247CFF);
+  static const Color textGrey = Color(0xFF757575);
+  static const Color hintText = Color(0xFFC2C2C2);
+  static const Color rememberMe = Color(0xFF9E9E9E);
+  static const Color loginOptions = Color(0xFFF5F5F5);
+  static const Color flagArrow = Color(0xFFF5F5F5);
+  static const Color loginBlack = Color(0xFF242424);
+}
