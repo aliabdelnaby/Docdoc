4385864671163593b0307220483577d09fe6866c
aliabdelnaby <aliabdelnabi239@gmail.com>
2024-08-10 17:40:32 +0300

    App Styles

---
+++ lib/core/utils/app_text_styles.dart
@@ -0,0 +1,45 @@
+import 'package:docdoc/core/utils/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class AppStyles {
+  static const TextStyle style32W700 = TextStyle(
+    color: AppColors.primary,
+    fontSize: 32,
+    fontWeight: FontWeight.w700,
+  );
+  static const TextStyle style14W400 = TextStyle(
+    color: AppColors.textGrey,
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle style16W600 = TextStyle(
+    color: AppColors.white,
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+  );
+  static const TextStyle style24W700 = TextStyle(
+    color: AppColors.primary,
+    fontSize: 24,
+    fontWeight: FontWeight.w700,
+  );
+  static const TextStyle style12W400 = TextStyle(
+    color: AppColors.rememberMe,
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle style11W400 = TextStyle(
+    color: AppColors.rememberMe,
+    fontSize: 11,
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle style11W500 = TextStyle(
+    color: AppColors.loginBlack,
+    fontSize: 11,
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle style11W600 = TextStyle(
+    color: AppColors.primary,
+    fontSize: 11,
+    fontWeight: FontWeight.w600,
+  );
+}
