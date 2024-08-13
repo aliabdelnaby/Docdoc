dc0f05bbcc680d2a46c9287ce49b3c2f66d7ff41
aliabdelnaby <aliabdelnabi239@gmail.com>
2024-08-13 21:58:43 +0300

    Make An Appointment Btn (bottomNavigationBar)

--- lib/features/home/presentation/views/doctor_details_view.dart
+++ lib/features/home/presentation/views/doctor_details_view.dart
@@ -1,3 +1,6 @@
+import 'package:docdoc/core/utils/app_colors.dart';
+import 'package:docdoc/core/utils/app_text_styles.dart';
+import 'package:docdoc/features/home/presentation/widgets/doctor_details/doctor_details_view_body.dart';
 import 'package:docdoc/features/home/presentation/widgets/notification/notifaction_app_bar.dart';
 import 'package:docdoc/features/home/presentation/widgets/recommendation_doctor/recommendation_doctor_more_btn.dart';
 import 'package:flutter/material.dart';
@@ -13,8 +16,36 @@ class DoctorDetailsView extends StatelessWidget {
         title: "Doctor Name",
         trailing: const CustomMoreBtn(),
       ),
-      body: const Column(
-        children: [],
+      bottomNavigationBar: MakeAnAppointmentBtn(
+        onPressed: () {},
+      ),
+      body: const DoctorDetailsViewBody(),
+    );
+  }
+}
+
+class MakeAnAppointmentBtn extends StatelessWidget {
+  const MakeAnAppointmentBtn({super.key, this.onPressed});
+  final void Function()? onPressed;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: AppColors.white,
+      margin:
+          const EdgeInsetsDirectional.symmetric(horizontal: 24, vertical: 16),
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          fixedSize: const Size(double.infinity, 52),
+          backgroundColor: AppColors.primary,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+          ),
+        ),
+        child: const Text(
+          "Make An Appointment",
+          style: AppStyles.style16W600,
+        ),
       ),
     );
   }
---
+++ lib/features/home/presentation/widgets/doctor_details/doctor_details_view_body.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DoctorDetailsViewBody extends StatelessWidget {
+  const DoctorDetailsViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [],
+    );
+  }
+}
