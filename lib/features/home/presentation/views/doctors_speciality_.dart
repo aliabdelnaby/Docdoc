7c2b9c0391adff1a7349dc89c6f98af761b8a538
aliabdelnaby <aliabdelnabi239@gmail.com>
2024-08-14 18:00:27 +0300

    Finish Display Doctor By Specialization

--- lib/features/home/presentation/views/doctors_speciality_.dart
+++ lib/features/home/presentation/views/doctors_speciality_.dart
@@ -1,3 +1,6 @@
+import 'package:docdoc/core/utils/app_colors.dart';
+import 'package:docdoc/core/utils/app_text_styles.dart';
+import 'package:docdoc/features/home/data/datasource/reecommendation_doctor_item_list_model.dart';
 import 'package:docdoc/features/home/data/models/specialization_response_model/specialization_response_model.dart';
 import 'package:docdoc/features/home/presentation/widgets/notification/notifaction_app_bar.dart';
 import 'package:flutter/material.dart';
@@ -18,7 +21,85 @@ class DoctorsSpeciality extends StatelessWidget {
         context,
         title: specialization.name!,
       ),
-      body: Container(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsetsDirectional.all(16),
+          child: Column(
+            children: List.generate(
+              specialization.doctors!.length,
+              (index) {
+                return InkWell(
+                  onTap: () {
+                    // context.push(
+                    //   '/doctorDetailsView',
+                    //   extra: {
+                    //     'doctor': state.specializations
+                    //         .specializations[index].doctors![0],
+                    //     'image': reecommendationDoctorList[index].image,
+                    //     'rating': reecommendationDoctorList[index]
+                    //         .rateAndReviews,
+                    //   },
+                    // );
+                  },
+                  child: Padding(
+                    padding:
+                        const EdgeInsetsDirectional.only(start: 8, bottom: 24),
+                    child: Row(
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadiusDirectional.circular(12),
+                          child: Image.asset(
+                            reecommendationDoctorList.elementAt(index).image,
+                            fit: BoxFit.cover,
+                            height: 110,
+                            width: 110,
+                          ),
+                        ),
+                        const SizedBox(width: 16),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                specialization.doctors![index].name!,
+                                style: AppStyles.style16W700,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                              const SizedBox(height: 8),
+                              Text(
+                                "${specialization.name} | ${specialization.doctors![index].degree}",
+                                style: AppStyles.style12W500,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                              const SizedBox(height: 12),
+                              Row(
+                                children: [
+                                  const Icon(
+                                    Icons.star,
+                                    color: AppColors.ratingStart,
+                                    size: 16,
+                                  ),
+                                  const SizedBox(width: 4),
+                                  Text(
+                                    reecommendationDoctorList
+                                        .elementAt(index)
+                                        .rateAndReviews,
+                                    style: AppStyles.style12W500,
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
