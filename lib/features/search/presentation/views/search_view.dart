c51765ee385ddea60960f53de3e676e99e97629f
aliabdelnaby <aliabdelnabi239@gmail.com>
2024-08-17 14:03:30 +0300

    Pass data from search item to doctor details view

--- lib/features/search/presentation/views/search_view.dart
+++ lib/features/search/presentation/views/search_view.dart
@@ -1,11 +1,10 @@
 import 'package:docdoc/core/utils/app_colors.dart';
 import 'package:docdoc/core/utils/app_text_styles.dart';
-import 'package:docdoc/features/home/data/datasource/reecommendation_doctor_item_list_model.dart';
-import 'package:docdoc/features/home/data/models/specialization_response_model/doctor.dart';
 import 'package:docdoc/features/home/presentation/cubit/home_cubit.dart';
 import 'package:docdoc/features/home/presentation/cubit/home_state.dart';
 import 'package:docdoc/features/home/presentation/widgets/notification/notifaction_app_bar.dart';
 import 'package:docdoc/features/home/presentation/widgets/recommendation_doctor/search_and_filter_bar.dart';
+import 'package:docdoc/features/search/presentation/widgets/search_item.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'dart:async';
@@ -28,7 +27,6 @@ class _SearchViewState extends State<SearchView> {
 
   void _onSearchChanged(String query) {
     if (_debounce?.isActive ?? false) _debounce?.cancel();
-
     _debounce = Timer(
       const Duration(milliseconds: 300),
       () {
@@ -86,7 +84,7 @@ class _SearchViewState extends State<SearchView> {
                         )
                       : state is SearchDoctorSuccessState &&
                               state.doctors.isNotEmpty
-                          ? _buildDoctorList(state.doctors)
+                          ? SearchItem(doctors: state.doctors)
                           : const Center(
                               child: Text("No results"),
                             ),
@@ -95,72 +93,6 @@ class _SearchViewState extends State<SearchView> {
             ),
           );
         },
-      ),
-    );
-  }
-
-  Widget _buildDoctorList(List<Doctor> doctors) {
-    return SingleChildScrollView(
-      child: Column(
-        children: List.generate(
-          doctors.length,
-          (index) {
-            var doctor = doctors[index];
-            return GestureDetector(
-              onTap: () {},
-              child: Padding(
-                padding: const EdgeInsetsDirectional.only(top: 16),
-                child: Row(
-                  children: [
-                    ClipRRect(
-                      borderRadius: BorderRadiusDirectional.circular(12),
-                      child: Image.asset(
-                        reecommendationDoctorList[index].image,
-                        fit: BoxFit.cover,
-                        height: 110,
-                        width: 110,
-                      ),
-                    ),
-                    const SizedBox(width: 16),
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            doctor.name!,
-                            style: AppStyles.style16W700,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                          const SizedBox(height: 8),
-                          Text(
-                            "${doctor.specialization!.name} | ${doctor.degree}",
-                            style: AppStyles.style12W500,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                          const SizedBox(height: 12),
-                          Row(
-                            children: [
-                              const Icon(
-                                Icons.star,
-                                color: AppColors.ratingStart,
-                                size: 16,
-                              ),
-                              const SizedBox(width: 4),
-                              Text(
-                                reecommendationDoctorList[index].rateAndReviews,
-                                style: AppStyles.style12W500,
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            );
-          },
-        ),
       ),
     );
   }
---
+++ lib/features/search/presentation/widgets/search_item.dart
@@ -0,0 +1,86 @@
+import 'package:docdoc/core/utils/app_colors.dart';
+import 'package:docdoc/core/utils/app_text_styles.dart';
+import 'package:docdoc/features/home/data/datasource/reecommendation_doctor_item_list_model.dart';
+import 'package:docdoc/features/home/data/models/specialization_response_model/doctor.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class SearchItem extends StatelessWidget {
+  const SearchItem({super.key, required this.doctors});
+  final List<Doctor> doctors;
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: List.generate(
+          doctors.length,
+          (index) {
+            var doctor = doctors[index];
+            return GestureDetector(
+              onTap: () {
+                context.push(
+                  '/doctorDetailsView',
+                  extra: {
+                    'doctor': doctor,
+                    'image': reecommendationDoctorList[index].image,
+                    'rating': reecommendationDoctorList[index].rateAndReviews,
+                  },
+                );
+              },
+              child: Padding(
+                padding: const EdgeInsetsDirectional.only(top: 16),
+                child: Row(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadiusDirectional.circular(12),
+                      child: Image.asset(
+                        reecommendationDoctorList[index].image,
+                        fit: BoxFit.cover,
+                        height: 110,
+                        width: 110,
+                      ),
+                    ),
+                    const SizedBox(width: 16),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            doctor.name!,
+                            style: AppStyles.style16W700,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          const SizedBox(height: 8),
+                          Text(
+                            "${doctor.specialization!.name} | ${doctor.degree}",
+                            style: AppStyles.style12W500,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          const SizedBox(height: 12),
+                          Row(
+                            children: [
+                              const Icon(
+                                Icons.star,
+                                color: AppColors.ratingStart,
+                                size: 16,
+                              ),
+                              const SizedBox(width: 4),
+                              Text(
+                                reecommendationDoctorList[index].rateAndReviews,
+                                style: AppStyles.style12W500,
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
